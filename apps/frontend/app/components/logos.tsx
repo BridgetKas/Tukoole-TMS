48be3e45dfca37d03bbb42fa82f1f4c944a9557d
Bridget <bridgetnamugga@gmail.com>
2026-02-12 13:44:08 +0300

    feat(frontend):Added logos section

--- apps/frontend/app/components/logos.tsx
+++ apps/frontend/app/components/logos.tsx
@@ -14,7 +14,7 @@ const logosArray:string[] = [
 export default function LogosSection(){
     return (
         <div className="bg-linear-to-b from-purple-50 to-white">
-            <p className="text-center px-2 pt-4 pb-9 text-gray-900 leading-0">We are trusted by startups, government agencies and NGOs worldwide to deliver exceptional talent.</p>
+            <p className="text-center px-2 pt-8 pb-9 text-gray-900">We are trusted by startups, government agencies and NGOs worldwide to deliver exceptional talent.</p>
             <div className="flex items-center justify-center flex-wrap px-3 gap-4 md:justify-between md:px-5">
                 {
                     logosArray.map((logo,index) =>{
